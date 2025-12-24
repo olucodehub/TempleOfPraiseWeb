3951156b4b9aa25b1461a90d27c860f6ca189559
Olu-Bunmi <48118599+Olu-Bunmi@users.noreply.github.com>
2025-12-24 11:45:35 +0100

    update join us page hero image

--- src/pages/NewMember.tsx
+++ src/pages/NewMember.tsx
@@ -38,7 +38,7 @@ const NewMember = () => {
       <div
         className="relative h-[400px] bg-cover bg-center"
         style={{
-          backgroundImage: 'url("https://images.unsplash.com/photo-1519326882834-04c334752f58?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80")',
+          backgroundImage: 'url("https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80")',
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-60" />
