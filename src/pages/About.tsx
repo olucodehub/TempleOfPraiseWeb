d59f119a02728bcb3e352ec7ea17f75bc17a4c4c
Olu-Bunmi <48118599+Olu-Bunmi@users.noreply.github.com>
2025-12-24 11:46:45 +0100

    update about us page hero image

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
       <div
         className="relative h-[400px] bg-cover bg-center"
         style={{
-          backgroundImage: "url('/images/about/header.png')",
+          backgroundImage: "url('https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80')",
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-60" />
